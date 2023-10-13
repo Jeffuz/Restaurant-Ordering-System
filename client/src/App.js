581367aeea61a605a16905a54bfbeeb7f86d7b7a
Jeffuz <jeffzhang0049@gmail.com>
2023-10-13 11:10:15 -0700

    setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route, HashRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <HashRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
