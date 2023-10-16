cdabb5c7509cd8d93c8c634c34e50cb0934df0d7
Jeffuz <jeffzhang0049@gmail.com>
2023-10-16 16:32:30 -0700

    lazy loading

--- client/src/components/itemCard.jsx
+++ client/src/components/itemCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ItemCard = ({ image, itemName, itemPrice }) => {
     return (
         <div className="bg-white rounded-lg overflow-hidden shadow-md">
-            <img src={image} alt={itemName} className="w-full h-64 object-cover" />
+            <img src={image} alt={itemName} className="w-full h-64 object-cover" loading="lazy"/>
             <div className="p-6">
                 <div className="text-lg font-semibold">{itemName}</div>
                 <div className="text-gray-600">${itemPrice}</div>
