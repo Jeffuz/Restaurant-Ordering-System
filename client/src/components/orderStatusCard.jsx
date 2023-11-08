f49051443578d3fa8b321d6c26143a6a1b3f1262
Jeffuz <jeffzhang0049@gmail.com>
2023-11-08 13:09:37 -0800

    create order cards for order status

--- client/src/components/orderStatusCard.jsx
+++ client/src/components/orderStatusCard.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const OrderStatusCard = ({ itemImage, itemName, itemOrderNum, itemCount, itemStatus }) => {
     return (
-        <div>
-            <img src={itemImage} alt={itemName} />
-            <div>Name: {itemName}</div>
-            <div>Order Number: #{itemOrderNum}</div>
-            <div>Count: {itemCount}</div>
-            <div>Status: {itemStatus}</div>
+        <div className="bg-gray-100 p-4 mb-4 flex items-center justify-between rounded-lg shadow-md">
+            <div className="mr-4">
+                <img src={itemImage} alt={itemName} className="h-16 object-cover" />
+            </div>
+            <div className="flex flex-col">
+                <div className="font-bold text-xl leading-none">{itemName}</div>
+                <div className="font-semibold text-xl leading-none">Order #{itemOrderNum}</div>
+                <div className="text-gray-500 font-normal text-sm leading-tight">Quantity: {itemCount}</div>
+            </div>
+            <div className="text-gray-500">{itemStatus}</div>
         </div>
     );
 }
