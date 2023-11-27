98f9c55190c252c792a9dcd4c66c26a880d1c968
Jeffuz <jeffzhang0049@gmail.com>
2023-11-27 14:34:05 -0800

    update color pallete and font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,9 +4,27 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ['Montserrat', 'sans'],//Define Montserrat font
+        montserrat: ['Montserrat', 'sans'], 
+        'tt-norms-pro': ['TT Norms Pro', 'sans-serif']
       },
-
+      colors: {
+        light: {
+          primary: '#395886',
+          secondary: '#638ECB',
+          tertiary: '#8AAEE0',
+          quaternary: '#B1C9EF',
+          quinary: '#D5DEEF',
+          senary: '#F0F3FA'
+        },
+        dark: {
+          primary: '#006DA4',
+          secondary: '#006494',
+          tertiary: '#004D74',
+          quaternary: '#003554',
+          quinary: '#022B42',
+          senary: '#032030'
+        },
+      },
     },
   },
   plugins: [],
