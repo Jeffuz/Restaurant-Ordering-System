8d3b1d5d01b3a375cbcfdb600cd8807de97ee8fc
Jeffuz <jeffzhang0049@gmail.com>
2023-11-29 13:45:16 -0800

    include ordermanager function for admin orders

--- client/src/pages/admin-orders.js
+++ client/src/pages/admin-orders.js
@@ -13,10 +13,7 @@ const Admin_orders = (props) => {
                 <div className="w-[80%] h-screen">
                     <div className='flex flex-col h-screen'>
                         <div className='text-center mt-27 text-black font-Montserrat text-4xl font-bold py-6'>Orders</div>
-
-                        <div>{/* Component */}</div>
-
-
+                        <OrderManager/>
                     </div>
                 </div>
             </div>
