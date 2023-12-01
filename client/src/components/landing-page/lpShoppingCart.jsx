d6b95645817c7c84d32c67f62f6366b3e4649d5e
Jeffuz <jeffzhang0049@gmail.com>
2023-11-30 22:23:10 -0800

    fix overflow for shopping cart

--- client/src/components/landing-page/lpShoppingCart.jsx
+++ client/src/components/landing-page/lpShoppingCart.jsx
@@ -13,7 +13,7 @@ const LpShoppingCart = ({ isOpen, onClose, cartItems }) => {
                     <AiOutlineClose onClick={onClose} size={20} />
                 </button>
                 <div className='flex justify-center text-3xl p-3'>Shopping Cart</div>
-                <div className='h-screen'>
+                <div className='h-screen overflow-y-auto'>
                     <div className='grid grid-cols-3 gap-8 p-8'>
                         {cartItems.map((cartItem) => (
                             <ShoppingCartCard
@@ -26,7 +26,7 @@ const LpShoppingCart = ({ isOpen, onClose, cartItems }) => {
                     </div>
                 </div>
                 <div className='flex flex-col items-center justify-end'>
-                    <button className='font-bold leading-10 rounded-3xl mx-auto px-3 bg-light-secondary text-white mb-3'>
+                    <button className='font-bold leading-10 rounded-3xl mx-auto px-3 bg-light-secondary text-white mb-3 mt-5'>
                         <div className='flex items-center gap-1'>
                             <FaShoppingCart size={20} />
                             Check Out
