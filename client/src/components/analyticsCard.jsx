2edb5d3939a52d31cbf4b8e019c87bab5a50fb35
ipsitabisht <ipsitabisht@gmail.com>
2023-12-03 14:03:48 -0800

    working on figuring out layout

--- client/src/components/analyticsCard.jsx
+++ client/src/components/analyticsCard.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const AnalyticsCard = ({description, icon, stat}) => {
 
     return (
-        <div className="flex flex-col bg-white items-center justify-center rounded-3xl m-4 p-2">
+        <div className="flex flex-col bg-white items-center justify-content rounded-3xl m-4 p-2 text-center">
             <p className="text-gray-500 text-sm mb-1">{description}</p>
             
-            <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-row items-center justify-center p-6">
                 <div className="mr-2 xl:text-5xl">{icon}</div>
                 
                 <p className="xl:text-5xl">{stat}</p>
--- client/src/components/analyticsManager.jsx
+++ client/src/components/analyticsManager.jsx
@@ -7,6 +7,7 @@ import AnalyticsCard from './analyticsCard'
 import { MdFastfood } from "react-icons/md";
 import { MdAttachMoney } from "react-icons/md";
 import { MdPeopleAlt } from "react-icons/md";
+import { FaMoneyBillWave } from "react-icons/fa";
 const Analytics = () => {
 
   const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF', '#FF4D4F', '#36CFC9'];
@@ -16,7 +17,11 @@ const Analytics = () => {
   // const handleTimePeriodChange = (newTimePeriod) => {
   //   setSelectedTimePeriod(newTimePeriod);
   // };
+
+  
   //mock data for fake restaurant containing menu , tables, and history
+
+  
   const [restaurantData, setRestaurantData] = useState({
     restaurantName: 'Your Restaurant',
     restaurantMenu: {
@@ -205,12 +210,111 @@ const Analytics = () => {
           // Add more order histories as needed
         ],
       },
+      {
+        userId: 'use6',
+        userHistory: [
+          {
+            menuItemId: '2',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2021-01-02T10:45:00'), 
+          },
+          {
+            menuItemId: '1',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2023-01-02T10:45:00'), 
+          },
+          {
+            menuItemId: '3',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2023-01-02T10:45:00'), 
+          },
+        
+          // Add more order histories as needed
+        ],
+      },
+
+      {
+        userId: 'user7',
+        userHistory: [
+          {
+            menuItemId: '2',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2021-01-02T13:45:00'), 
+          },
+          {
+            menuItemId: '1',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2023-01-02T13:45:00'), 
+          },
+   
+       
+          // Add more order histories as needed
+        ],
+      },
+      
+      {
+        userId: 'user9',
+        userHistory: [
+          {
+            menuItemId: '2',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2021-01-02T13:45:00'), 
+          },
+          {
+            menuItemId: '1',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2023-01-02T13:45:00'), 
+          },
+   
+       
+          // Add more order histories as needed
+        ],
+      },
+      {
+        userId: 'user10',
+        userHistory: [
+          {
+            menuItemId: '2',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2021-01-02T13:45:00'), 
+          },
+          {
+            menuItemId: '1',
+            quantity: 3,
+            custom: ['extra sauce'],
+            price: 30,
+            createdAt: new Date('2023-01-02T13:45:00'), 
+          },
+   
+       
+          // Add more order histories as needed
+        ],
+      },
       // Add more user data as needed
     ],
   });
 
 
-
+  if (!restaurantData) {
+   
+    return <p>Loading...</p>;
+  }
   //total orders across all users
   const calculateTotalOrders = () => {
     if (!restaurantData || !restaurantData.history) {
@@ -323,67 +427,75 @@ const Analytics = () => {
 
   const totalCustomers = calculateTotalCustomers();
 
+  const avgCustomerSpending = totalCustomers > 0 ? (totalSales / totalCustomers).toFixed(2) : 0;
+
   const popularTimesData = calculatePopularTimes();
+  const sortedPopularTimesData = popularTimesData.sort((a, b) => b.count - a.count);
 
   return (
     <div>
 
-      <div className="flex flex-row items-center justify-content">
 
-        <div>
-          <AnalyticsCard description='Total Orders' icon={<MdFastfood />} stat={totalOrders}/>
-          <AnalyticsCard description='Total Sales' icon={<MdAttachMoney />} stat={totalSales}/>
-          <AnalyticsCard description='Total Users' icon={<MdPeopleAlt />} stat={totalCustomers}/>
+      <div className="flex flex-col">
+        <div className="flex flex-row items-center justify-content">
 
+            <div className="grid grid-cols-2 space-4">
+              <AnalyticsCard description='Total Orders' icon={<MdFastfood />} stat={totalOrders}/>
+              <AnalyticsCard description='Total Sales' icon={<MdAttachMoney />} stat={totalSales}/>
+              <AnalyticsCard description='Total Users' icon={<MdPeopleAlt />} stat={totalCustomers}/>
+              <AnalyticsCard description='Average Customer Spending' icon={<FaMoneyBillWave />} stat={avgCustomerSpending}/>
+
+
+            </div>
+
+
+
+            <div className="grid grid-cols-2 space-4">
+              <div className="bg-white rounded-3xl m-4 p-2 flex flex-col items-center justify-content">
+                <p className="text-2xl">Popular menu items</p>
+                <PieChart  width={400} height={400} className="mx-auto">
+                  <Pie
+                    data={menuItemCounts}
+                    dataKey="count"
+                    nameKey="menuItemId"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                  >
+                    {menuItemCounts.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                  </Pie>
+                  <Tooltip content={({ payload }) => <div>Total: {payload[0]?.payload.menuItemId}</div>} />
+                  <Legend />
+                </PieChart>
+              </div>
+            </div>
+          </div>
+
+        
+
+          <div className="bg-white rounded-3xl m-4 p-2 flex flex-col items-center justify-between">
+                <p className="text-2xl">Most to Least Popular Times</p>
+                <BarChart width={400} height={300} data={sortedPopularTimesData}>
+                  <XAxis dataKey="hour" 
+                        tickFormatter={(value) => {
+                          const originalHour = parseInt(value, 10);
+                          const formattedHour = originalHour % 12 || 12; // Convert to 12-hour format
+                          const ampm = originalHour >= 12 ? 'PM' : 'AM';
+                          return `${formattedHour} ${ampm}`;
+                        }}
+                  />
+                  <Bar dataKey="count" fill={COLORS[0]} />
+                </BarChart>
+
+          </div>
 
         </div>
       
-
-        <div className="bg-white rounded-3xl m-4 p-2">
-          <p>Popular menu items</p>
-          <PieChart  width={400} height={400} className="mx-auto">
-            <Pie
-              data={menuItemCounts}
-              dataKey="count"
-              nameKey="menuItemId"
-              cx="50%"
-              cy="50%"
-              outerRadius={80}
-              fill="#8884d8"
-              label
-            >
-              {menuItemCounts.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-            <Tooltip content={({ payload }) => <div>Total: {payload[0]?.payload.menuItemId}</div>} />
-            
-          </PieChart>
-
-
-
-        </div>
-        <div className="bg-white rounded-3xl m-4 p-2">
-          <p>Popular times</p>
-          <BarChart width={400} height={300} data={popularTimesData}>
-            <XAxis dataKey="hour" 
-                  tickFormatter={(value) => {
-                    const originalHour = parseInt(value, 10);
-                    const formattedHour = originalHour % 12 || 12; // Convert to 12-hour format
-                    const ampm = originalHour >= 12 ? 'PM' : 'AM';
-                    return `${formattedHour} ${ampm}`;
-                  }}
-            />
-
-     
-            <Legend />
-            <Bar dataKey="count" fill={COLORS[0]} />
-          </BarChart>
-        </div>
-     
-     
-      </div>
-      
+        
     </div>
   );
 
