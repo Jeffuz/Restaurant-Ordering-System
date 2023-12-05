700ff8e309045c25b584a4f1b38080565e9c9647
Jeffuz <jeffzhang0049@gmail.com>
2023-12-04 21:57:15 -0800

    fix ui for order status cards

--- client/src/pages/admin-orders.js
+++ client/src/pages/admin-orders.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import AdminNavbar from '../components/adminNavbar'
 import OrderManager from '../components/orderManager'
+import AdminOrderStatusCard from '../components/adminOrderStatusCard';
 
 const Admin_orders = (props) => {
 
@@ -57,17 +58,21 @@ const Admin_orders = (props) => {
                 <div className="w-[80%] h-screen">
                     <div className='flex flex-col h-screen'>
                         <div className='text-center h-[10%] text-black font-Montserrat text-4xl font-bold py-6 '>Orders</div>
-
                         <div className='px-6 pb-6 h-[90%]'>
                             <div className='flex flex-row gap-6 h-full'>
                                 <div className='flex flex-col w-full bg-white rounded-2xl'>
                                     <div className='flex justify-center p-3 leading-10 font-bold text-2xl'>Order's Waiting</div>
                                     <div className='flex flex-col overflow-y-auto'>
                                         {waitingOrders.map((waitingOrder) => (
-                                            <button onClick={() => workOnItem(waitingOrder)} className='pb-3'>
-                                                client: {waitingOrder.orderer} <br />
+                                            <button onClick={() => workOnItem(waitingOrder)} className='mx-5'>
+                                                {/* client: {waitingOrder.orderer} <br />
                                                 Order id: {waitingOrder.hash} <br />
-                                                Order: {waitingOrder.item.id}
+                                                Order: {waitingOrder.item.id} */}
+                                                <AdminOrderStatusCard
+                                                    itemName={waitingOrder.item.itemName}
+                                                    itemImage={waitingOrder.item.image}
+                                                    orderId={waitingOrder.hash}
+                                                />
                                             </button>
                                         ))}
                                     </div>
@@ -78,11 +83,12 @@ const Admin_orders = (props) => {
                                     </div>
                                     <div className='flex flex-col overflow-y-auto'>
                                         {workingOrders.map((workingOrder) => (
-                                            <button onClick={() => finishItem(workingOrder)} className='mb-3'>
-                                                client: {workingOrder.orderer} <br />
-                                                Order id: {workingOrder.hash} <br />
-                                                Order: {workingOrder.item.id} <br />
-                                                <br />
+                                            <button onClick={() => finishItem(workingOrder)} className='mx-5'>
+                                                <AdminOrderStatusCard
+                                                    itemName={workingOrder.item.itemName}
+                                                    itemImage={workingOrder.item.image}
+                                                    orderId={workingOrder.hash}
+                                                />
                                             </button>
                                         ))}
                                     </div>
@@ -91,10 +97,12 @@ const Admin_orders = (props) => {
                                     <div className='flex justify-center p-3 leading-10 font-bold text-2xl'>Finished Order's</div>
                                     <div className='flex flex-col overflow-y-auto'>
                                         {finishedOrders.map((finishedOrder) => (
-                                            <button className='pb-3'>
-                                                client: {finishedOrder.orderer} <br />
-                                                Order id: {finishedOrder.hash} <br />
-                                                Order: {finishedOrder.item.id}
+                                            <button className='mx-5'>
+                                                <AdminOrderStatusCard
+                                                    itemName={finishedOrder.item.itemName}
+                                                    itemImage={finishedOrder.item.image}
+                                                    orderId={finishedOrder.hash}
+                                                />
                                             </button>
                                         ))}
                                     </div>
