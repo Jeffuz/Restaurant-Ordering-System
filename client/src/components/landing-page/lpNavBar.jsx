1b00ff885016e405d8da701a08bb92049f56da19
Jeffuz <jeffzhang0049@gmail.com>
2023-12-04 22:27:04 -0800

    Revert "Merge pull request #32 from Jeffuz/login-eunice"
    This reverts commit 41d91f8bab78df0340422fa6ba53b2027f2f207a, reversing
    changes made to 49ba0678a61fe1d18314379011c82c5452c8c57f.

--- client/src/components/landing-page/lpNavBar.jsx
+++ client/src/components/landing-page/lpNavBar.jsx
@@ -1,65 +1,98 @@
-import React, { useState, useEffect } from "react";
-import Login from "../login";
-import Signup from "../signup";
-import { IoCartOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import Login from '../login';
+import Signup from '../signup';
+import { IoCartOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 import { MdOutlineDashboard } from "react-icons/md";
-import LpShoppingCart from "./lpShoppingCart";
-import { auth, readUserData } from "../../firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-
+import LpShoppingCart from './lpShoppingCart';
 
 const LpNavBar = ({cartItems, cartSize, renderCartSize, removeFromCart, WebSocketService}) => {
     /* Testing */
-    // let email = true;
+    let email = true;
     // let email = false;
-    // let admin = true;
+    let admin = true;
     // let admin = false;
 
-    const [email, setEmail] = useState(false);
-    const [admin, setAdmin] = useState(false);
-    const [signupSuccess, setSignupSuccess] = useState(false);
-    const [signinSuccess, setSigninSuccess] = useState(false);
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        // Define an async function to handle user authentication
-        const handleAuthChange = async (user) => {
-            if (user) {
-                setEmail(user.email);
-
-                try {
-                    const userData = await readUserData(user.uid);
-                    setAdmin(userData.restaurantId !== 0);
-                } catch (error) {
-                    console.error(error);
-                }
-            } else {
-                setEmail(null);
-                setAdmin(false);
-            }
-        };
-
-        const unsubscribe = onAuthStateChanged(auth, handleAuthChange);
-
-        return () => unsubscribe();
-    }, [auth]);
-
-    const handleLogout = () => {
-        signOut(auth)
-            .then(() => {
-                setEmail(false);
-                setAdmin(false);
-                setSigninSuccess(false);
-                setSignupSuccess(false);
-                navigate("/");
-            })
-            .catch((error) => {
-                console.error("Logout error:", error);
-            });
-    };
+    /*const cartItems = [
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 1
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+        {
+            itemImage: 'test/nacho-chips.png',
+            itemName: 'Nacho chips',
+            itemPrice: 9.99,
+            itemCount: 2
+        },
+    ];*/
 
     // Sign In/Up
     const [selectedLoginItem, setSelectedLoginItem] = useState(null);
@@ -94,42 +127,30 @@ const LpNavBar = ({cartItems, cartSize, renderCartSize, removeFromCart, WebSocke
 
     return (
         <>
-            <div className="font-tt-norms-pro px-12 relative z-20">
-                <div className="md:flex absolute items-center font-bold text-4xl text-light-secondary hidden top-4 left-8 ">
+            <div className='font-tt-norms-pro px-12 relative z-20'>
+                <div className='md:flex absolute items-center font-bold text-4xl text-light-secondary hidden top-4 left-8 '>
                     <Link to={"/"}>115A's Diner</Link>
                 </div>
                 {email ? (
-                    <div className="absolute top-4 right-8 ">
-                        <div className="flex font-bold items-center gap-3 ">
+                    <div className='absolute top-4 right-8 '>
+                        <div className='flex font-bold items-center gap-3 '>
                             {admin && (
                                 <Link to="/admin-dashboard">
                                     <MdOutlineDashboard size={30} />
                                 </Link>
                             )}
-                            <div className="flex gap-3 items-center ">
-                                <button
-                                    className="flex items-center text-black rounded-3xl leading-10 px-3"
-                                    onClick={() => openCartModal()}
-                                >
+                            <div className='flex gap-3 items-center '>
+                                <button className='flex items-center text-black rounded-3xl leading-10 px-3' onClick={() => openCartModal()}>
                                     <IoCartOutline size={30} />
                                     {/* Adjust Item Count here */}
-
                                     {cartSize > 0 && <span className='ml-1'>{itemCount}</span>}
-
                                 </button>
-                                <Link to={`/profile`}>
-                                    <img
-                                        src={
-                                            auth.currentUser.photoURL ||
-                                            "profile.png"
-                                        }
-                                        alt="Profile"
-                                        className="w-10 h-10 rounded-full"
-                                    />
+                                <Link to="" >
+                                    <img src="profile.png" alt='Profile' className='w-10 h-10 rounded-full' />
                                 </Link>
                                 <button
-                                    className="px-3 text-black rounded-3xl leading-10"
-                                    onClick={handleLogout}
+                                    className='px-3 text-black rounded-3xl leading-10'
+                                // onClick = {}
                                 >
                                     Logout
                                 </button>
@@ -137,46 +158,23 @@ const LpNavBar = ({cartItems, cartSize, renderCartSize, removeFromCart, WebSocke
                         </div>
                     </div>
                 ) : (
-                    <div className="absolute top-4 right-8">
-                        <div className="flex gap-x-2 font-bold">
-                            <button
-                                className="px-3 text-black rounded-3xl leading-10"
-                                onClick={() => openLoginModal()}
-                            >
+                    <div className='absolute top-4 right-8'>
+                        <div className='flex gap-x-2 font-bold'>
+                            <button className='px-3 text-black rounded-3xl leading-10' onClick={() => openLoginModal()}>
                                 Sign In
                             </button>
-                            <button
-                                className="px-3 bg-light-secondary rounded-3xl leading-10 text-white"
-                                onClick={() => openSignupModal()}
-                            >
+                            <button className='px-3 bg-light-secondary rounded-3xl leading-10 text-white' onClick={() => openSignupModal()}>
                                 Sign Up
                             </button>
                         </div>
                     </div>
                 )}
             </div>
-
-            <Login
-                isOpen={selectedLoginItem !== null}
-                onClose={closeLoginModal}
-                signinSuccess={signinSuccess}
-                setSigninSuccess={setSigninSuccess}
-            />
-            <Signup
-                isOpen={selectedSignupItem !== null}
-                onClose={closeSignupModal}
-                signupSuccess={signupSuccess}
-                setSignupSuccess={setSignupSuccess}
-            />
-            <LpShoppingCart
-                isOpen={selectedCartItem !== null}
-                onClose={closeCartModal}
-                cartItems={cartItems}
-            />
+            <Login isOpen={selectedLoginItem !== null} onClose={closeLoginModal} />
+            <Signup isOpen={selectedSignupItem !== null} onClose={closeSignupModal} />
             <LpShoppingCart isOpen={selectedCartItem !== null} onClose={closeCartModal} cartItems={cartItems} cartSize={cartSize} renderCartSize={renderCartSize} removeFromCart={removeFromCart} WebSocketService={WebSocketService}/>
-
         </>
-    );
-};
+    )
+}
 
-export default LpNavBar;
+export default LpNavBar
