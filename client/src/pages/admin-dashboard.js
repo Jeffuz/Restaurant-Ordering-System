9461fcbb3e07125a368fff7e7ddb8f7a0838b349
Jeffuz <jeffzhang0049@gmail.com>
2023-12-06 00:52:36 -0800

    loading svg

--- client/src/pages/admin-dashboard.js
+++ client/src/pages/admin-dashboard.js
@@ -25,7 +25,60 @@ const Admin_dashboard = (props) => {
   }, []);
 
   if (loading) {
-    return <div>loading...</div>;
+    return (
+      <div className="flex justify-center item-center h-screen">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+          <radialGradient
+            id="a12"
+            cx=".66"
+            fx=".66"
+            cy=".3125"
+            fy=".3125"
+            gradientTransform="scale(1.5)"
+          >
+            <stop offset="0" stop-color="#FE0435"></stop>
+            <stop offset=".3" stop-color="#FE0435" stop-opacity=".9"></stop>
+            <stop offset=".6" stop-color="#FE0435" stop-opacity=".6"></stop>
+            <stop offset=".8" stop-color="#FE0435" stop-opacity=".3"></stop>
+            <stop offset="1" stop-color="#FE0435" stop-opacity="0"></stop>
+          </radialGradient>
+          <circle
+            transform-origin="center"
+            fill="none"
+            stroke="url(#a12)"
+            stroke-width="15"
+            stroke-linecap="round"
+            stroke-dasharray="200 1000"
+            stroke-dashoffset="0"
+            cx="100"
+            cy="100"
+            r="70"
+          >
+            <animateTransform
+              type="rotate"
+              attributeName="transform"
+              calcMode="spline"
+              dur="2"
+              values="360;0"
+              keyTimes="0;1"
+              keySplines="0 0 1 1"
+              repeatCount="indefinite"
+            ></animateTransform>
+          </circle>
+          <circle
+            transform-origin="center"
+            fill="none"
+            opacity=".2"
+            stroke="#FE0435"
+            stroke-width="15"
+            stroke-linecap="round"
+            cx="100"
+            cy="100"
+            r="70"
+          ></circle>
+        </svg>
+      </div>
+    );
   } else if (page === "Dashboard") {
     return (
       <>
